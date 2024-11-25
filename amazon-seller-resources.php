2f4ddf79b1c784588b7c0b9481d9d7937688d15c
cr8nvgitdemo <cr@8nvconsulting.com>
2024-11-26 00:15:47 +0530

    Amazon seller page added

---
+++ amazon-seller-resources.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fulfillment Ninjas</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <link rel="stylesheet" type="text/css" href="slick/slick.css"/>
+    <link rel="stylesheet" type="text/css" href="slick/slick-theme.css"/>
+
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+  
+    <?php require 'includes/header.php'; ?>
+
+    <section class="pattern-block">
+        <div class="container banner">
+            <h1>Amazon Seller Resources</h1>
+            <p class="lead">Take your Amazon business to the next level by <br/> taking advantage of the top industry tools today.</p>            
+        </div>
+    </section>
+
+    <section class="amazon-resources">
+        <div class="container banner text-center">
+            <h1>Popular Categories</h1>
+            <p>We've created this page to serve as a resource guide for Amazon Sellers. There's many different categories here consisting of major tools and services Amazon sellers might use. Click on any category to view the listings in that specific category.</p>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-lg-4">
+                    <aside>
+                        <i class="fa-solid fa-bullseye"></i>
+                        <p>200+ FBA Prep Centers from USA, UK, Canada and China.</p>
+                        <h5>FBA Prep Centers</h5>
+                    </aside>
+                </div>
+                <div class="col-12 col-lg-4">
+                    <aside>
+                        <i class="fa-solid fa-chart-column"></i>
+                        <p>200+ FBA Prep Centers from USA, UK, Canada and China.</p>
+                        <h5>FBA Prep Centers</h5>
+                    </aside>
+                </div>
+                <div class="col-12 col-lg-4">
+                    <aside>
+                        <i class="fa-solid fa-file-lines"></i>
+                        <p>200+ FBA Prep Centers from USA, UK, Canada and China.</p>
+                        <h5>FBA Prep Centers</h5>
+                    </aside>
+                </div>
+                <div class="col-12 col-lg-4">
+                    <aside>
+                        <i class="fa-solid fa-bullseye"></i>
+                        <p>200+ FBA Prep Centers from USA, UK, Canada and China.</p>
+                        <h5>FBA Prep Centers</h5>
+                    </aside>
+                </div>
+                <div class="col-12 col-lg-4">
+                    <aside>
+                        <i class="fa-solid fa-bullseye"></i>
+                        <p>200+ FBA Prep Centers from USA, UK, Canada and China.</p>
+                        <h5>FBA Prep Centers</h5>
+                    </aside>
+                </div>
+                <div class="col-12 col-lg-4">
+                    <aside>
+                        <i class="fa-solid fa-bullseye"></i>
+                        <p>200+ FBA Prep Centers from USA, UK, Canada and China.</p>
+                        <h5>FBA Prep Centers</h5>
+                    </aside>
+                </div>
+                <div class="col-12 col-lg-4">
+                    <aside>
+                        <i class="fa-solid fa-chart-column"></i>
+                        <p>200+ FBA Prep Centers from USA, UK, Canada and China.</p>
+                        <h5>FBA Prep Centers</h5>
+                    </aside>
+                </div>
+                <div class="col-12 col-lg-4">
+                    <aside>
+                        <i class="fa-solid fa-file-lines"></i>
+                        <p>200+ FBA Prep Centers from USA, UK, Canada and China.</p>
+                        <h5>FBA Prep Centers</h5>
+                    </aside>
+                </div>
+                <div class="col-12 col-lg-4">
+                    <aside>
+                        <i class="fa-solid fa-bullseye"></i>
+                        <p>200+ FBA Prep Centers from USA, UK, Canada and China.</p>
+                        <h5>FBA Prep Centers</h5>
+                    </aside>
+                </div>
+                <div class="col-12 col-lg-4">
+                    <aside>
+                        <i class="fa-solid fa-bullseye"></i>
+                        <p>200+ FBA Prep Centers from USA, UK, Canada and China.</p>
+                        <h5>FBA Prep Centers</h5>
+                    </aside>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="pattern-block">
+        <div class="container">
+            <div class="row justify-content-between">
+                <div class="col-auto">
+                    <h1 class="text-start">Looking for a Fulfillment Center?</h1>
+                    <div class="tick-list">
+                        <ul class="d-flex gap-3">
+                            <li>No Contracts</li>
+                            <li>Dedicated Reps</li>
+                            <li>Instant Savings</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-auto">
+                    <a href="#" class="btn btn-primary btn-lg p-3 px-4">Contact Sales</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <?php require 'includes/footer.php'; ?>
+
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="slick/slick.min.js"></script>
+    
+    <script src="js/main.js"></script>
+  </body>
+</html>
