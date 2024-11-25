b77eb7e8eeb85ecae83cdcb4cc80bd8c93ff8c3c
cr8nvgitdemo <cr@8nvconsulting.com>
2024-11-26 01:51:21 +0530

    Legal stuff page added

---
+++ legal-stuff.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fulfillment Ninjas</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <link rel="stylesheet" type="text/css" href="slick/slick.css"/>
+    <link rel="stylesheet" type="text/css" href="slick/slick-theme.css"/>
+
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+  
+    <?php require 'includes/header.php'; ?>
+
+    <section class="amazon-resources pt-0">
+        <div class="container banner text-center">
+            <h1>Legal Stuff</h1>
+            <p>You can also email us: <a href="mailto:legal@shiphype.com">legal@shiphype.com</a></p>
+        </div>
+        <div class="container">
+            <div class="row">                
+                <div class="col-12 col-lg-4">
+                    <aside>
+                        <a href="#">
+                            <img src="images/Terms-of-Use.png" alt="">
+                            <h5>Terms of Service</h5>
+                            <p>Visit our Terms of Service page to learn more about our terms & conditions.</p>                            
+                        </a>
+                    </aside>
+                </div>
+                <div class="col-12 col-lg-4">
+                    <aside>
+                        <a href="#">
+                            <img src="images/Terms-of-Use.png" alt="">
+                            <h5>Terms of Service</h5>
+                            <p>Visit our Terms of Service page to learn more about our terms & conditions.</p>                            
+                        </a>
+                    </aside>
+                </div>
+                <div class="col-12 col-lg-4">
+                    <aside>
+                        <a href="#">
+                            <img src="images/Terms-of-Use.png" alt="">
+                            <h5>Terms of Service</h5>
+                            <p>Visit our Terms of Service page to learn more about our terms & conditions.</p>                            
+                        </a>
+                    </aside>
+                </div>
+                <div class="col-12 col-lg-4">
+                    <aside>
+                        <a href="#">
+                            <img src="images/Terms-of-Use.png" alt="">
+                            <h5>Terms of Service</h5>
+                            <p>Visit our Terms of Service page to learn more about our terms & conditions.</p>                            
+                        </a>
+                    </aside>
+                </div>
+                <div class="col-12 col-lg-4">
+                    <aside>
+                        <a href="#">
+                            <img src="images/Terms-of-Use.png" alt="">
+                            <h5>Terms of Service</h5>
+                            <p>Visit our Terms of Service page to learn more about our terms & conditions.</p>                            
+                        </a>
+                    </aside>
+                </div>
+                <div class="col-12 col-lg-4">
+                    <aside>
+                        <a href="#">
+                            <img src="images/Terms-of-Use.png" alt="">
+                            <h5>Terms of Service</h5>
+                            <p>Visit our Terms of Service page to learn more about our terms & conditions.</p>                            
+                        </a>
+                    </aside>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <?php require 'includes/footer.php'; ?>
+
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="slick/slick.min.js"></script>
+    
+    <script src="js/main.js"></script>
+  </body>
+</html>
