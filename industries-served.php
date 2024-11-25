31fa551801c8e55476d1033645c254d27706b048
cr8nvgitdemo <cr@8nvconsulting.com>
2024-11-26 01:31:41 +0530

    Industries-served page added

---
+++ industries-served.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fulfillment Ninjas</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <link rel="stylesheet" type="text/css" href="slick/slick.css"/>
+    <link rel="stylesheet" type="text/css" href="slick/slick-theme.css"/>
+
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+  
+    <?php require 'includes/header.php'; ?>
+
+    <div class="container banner">
+        <div class="row align-items-center">
+            <div class="col-12 col-lg-6">
+                <article class="pe-5">
+                    <div class="display-5 fw-bold">Industries We Serve</div>
+                    <p class="lead py-3">We specialize in fulfilling lightweight, fast-moving consumer goods across a variety of categories.</p>                    
+                </article>
+            </div>
+            <div class="col-12 col-lg-6">
+                <img src="images/Industries_serve.png" alt="">
+            </div>
+        </div>
+    </div>
+
+    <section class="pt-0">
+        <div class="container banner">
+            <h1>We serve clients across 50 different industries.</h1>
+        </div>
+        <div class="container industries-served">
+            <div class="row">
+                <div class="col-12 col-lg-4 mb-4">
+                    <aside class="flex-column">
+                        <img class="" src="images/Apparel.png" alt="">
+                        <h6 class="mt-3">Apparel</h6>
+                        <strong>
+                            <a href="#">Explore <i class="fa-solid fa-circle-arrow-right"></i></a>
+                        </strong>
+                    </aside>
+                </div>
+                <div class="col-12 col-lg-4 mb-4">
+                    <aside class="flex-column">
+                        <img class="" src="images/Apparel.png" alt="">
+                        <h6 class="mt-3">Apparel</h6>
+                        <strong>
+                            <a href="#">Explore <i class="fa-solid fa-circle-arrow-right"></i></a>
+                        </strong>
+                    </aside>
+                </div>
+                <div class="col-12 col-lg-4 mb-4">
+                    <aside class="flex-column">
+                        <img class="" src="images/Apparel.png" alt="">
+                        <h6 class="mt-3">Apparel</h6>
+                        <strong>
+                            <a href="#">Explore <i class="fa-solid fa-circle-arrow-right"></i></a>
+                        </strong>
+                    </aside>
+                </div>
+                <div class="col-12 col-lg-4 mb-4">
+                    <aside class="flex-column">
+                        <img class="" src="images/Apparel.png" alt="">
+                        <h6 class="mt-3">Apparel</h6>
+                        <strong>
+                            <a href="#">Explore <i class="fa-solid fa-circle-arrow-right"></i></a>
+                        </strong>
+                    </aside>
+                </div>
+                <div class="col-12 col-lg-4 mb-4">
+                    <aside class="flex-column">
+                        <img class="" src="images/Apparel.png" alt="">
+                        <h6 class="mt-3">Apparel</h6>
+                        <strong>
+                            <a href="#">Explore <i class="fa-solid fa-circle-arrow-right"></i></a>
+                        </strong>
+                    </aside>
+                </div>
+            </div>
+        </div>
+    </section>
+   
+    <section class="pattern-block banner">
+        <div class="container">
+            <h1>Ready to Get Started?</h1>
+            <a href="#" class="btn btn-primary btn-lg">Contact Sales</a>
+        </div>
+    </section>
+    
+    <?php require 'includes/footer.php'; ?>
+
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="slick/slick.min.js"></script>
+    
+    <script src="js/main.js"></script>
+  </body>
+</html>
