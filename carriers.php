3b34b29ccb518ff15da10d2f6b512a64422d7b58
cr8nvgitdemo <cr@8nvconsulting.com>
2024-11-25 23:08:23 +0530

    career page added

---
+++ carriers.php
@@ -0,0 +1,232 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fulfillment Ninjas</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
+        integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <link rel="stylesheet" type="text/css" href="slick/slick.css" />
+    <link rel="stylesheet" type="text/css" href="slick/slick-theme.css" />
+
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+    <?php require 'includes/header.php'; ?>
+
+    <div class="container banner">
+        <div class="row align-items-center">
+            <div class="col-12 col-lg-6">
+                <article class="pe-5">
+                    <div class="display-5 fw-bold">Carriers</div>
+                    <p class="lead pt-3 mb-0">We've teamed up with all the major carriers in North America to get you the best shipping rates.</p>
+                </article>
+            </div>
+            <div class="col-12 col-lg-6">
+                <div class="mb-4 mt-4 text-center">
+                    <a href="#" class="btn btn-secondary btn-lg px-4 py-3" style="font-size:22px;">See carriers list <i class="fa-solid fa-arrow-down"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <section class="shadow-box">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h6 class="carriers-title">CANADIAN CARRIERS</h6>
+                </div>
+                <div class="col-12 col-lg-3">
+                    <aside>
+                        <img src="images/feded_logo.png" alt="">
+                    </aside>
+                </div>
+                <div class="col-12 col-lg-3">
+                    <aside>
+                        <img src="images/ups_logo.png" alt="">
+                    </aside>
+                </div>
+                <div class="col-12 col-lg-3">
+                    <aside>
+                        <img src="images/dhl_logo.png" alt="">
+                    </aside>
+                </div>
+                <div class="col-12 col-lg-3">
+                    <aside>
+                        <img src="images/united_logo.png" alt="">
+                    </aside>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="shadow-box">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h6 class="carriers-title">US CARRIERS</h6>
+                </div>
+                <div class="col-12 col-lg-3">
+                    <aside>
+                        <img src="images/feded_logo.png" alt="">
+                    </aside>
+                </div>
+                <div class="col-12 col-lg-3">
+                    <aside>
+                        <img src="images/ups_logo.png" alt="">
+                    </aside>
+                </div>
+                <div class="col-12 col-lg-3">
+                    <aside>
+                        <img src="images/dhl_logo.png" alt="">
+                    </aside>
+                </div>
+                <div class="col-12 col-lg-3">
+                    <aside>
+                        <img src="images/united_logo.png" alt="">
+                    </aside>
+                </div>
+                <div class="col-12 col-lg-3">
+                    <aside>
+                        <img src="images/feded_logo.png" alt="">
+                    </aside>
+                </div>
+                <div class="col-12 col-lg-3">
+                    <aside>
+                        <img src="images/ups_logo.png" alt="">
+                    </aside>
+                </div>
+                <div class="col-12 col-lg-3">
+                    <aside>
+                        <img src="images/dhl_logo.png" alt="">
+                    </aside>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <div class="container banner">
+        <div class="row align-items-center">
+            <div class="col-12 col-lg-6">
+                <article class="pe-5">
+                    <div class="display-5 fw-bold">Access the Lowest Shipping Rates Available</div>
+                    <p class="lead py-3">Our aggregate volume allows us to get deep discounts from carriers. When using SHIPHYPE, you get access to these same rates.</p>
+                    <p>You can also connect your own shipping account so you can leverage your own negotiated rates.</p>
+                </article>
+            </div>
+            <div class="col-12 col-lg-6">
+                <img src="images/career-1.png" alt="">
+            </div>
+        </div>
+    </div>
+
+    <div class="container banner">
+        <div class="row align-items-center">
+            <div class="col-12 col-lg-6">
+                <img src="images/career-2.png" alt="">
+            </div>    
+            <div class="col-12 col-lg-6">
+                <article class="pe-5">
+                    <div class="display-5 fw-bold">Built-In Rate Shopping</div>
+                    <p class="lead py-3">Our software automatically compares rates across carriers, to ensure you pay the lowest price possible - for every shipment!</p>
+                    <p>See our pricing page to get a better idea of all costs involved, including shipping charges.</p>
+                </article>
+            </div>            
+        </div>
+    </div>
+
+    <section>
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <h2>Frequently Asked Questions</h2>
+            <p>See answers to some of the most common questions we get asked.</p>
+            <div class="accordion" id="accordionExample">
+              <div class="accordion-item">
+                <h2 class="accordion-header">
+                  <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                    Do you integrate with Shopify and Amazon?
+                  </button>
+                </h2>
+                <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+                  <div class="accordion-body">
+                    Yes, we integrate with both.
+                  </div>
+                </div>
+              </div>
+              <div class="accordion-item">
+                <h2 class="accordion-header">
+                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                    Do you offer Amazon FBA Prep Services?
+                  </button>
+                </h2>
+                <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                  <div class="accordion-body">
+                    Yes.
+                  </div>
+                </div>
+              </div>
+              <div class="accordion-item">
+                <h2 class="accordion-header">
+                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                    Where are your Fulfillment Centers Located?
+                  </button>
+                </h2>
+                <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                  <div class="accordion-body">
+                    We have locations in the United States. To see the exact addresses, click the 'Locations' button in the header.
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="pattern-block">
+        <div class="container">
+            <h1>Solutions for every Business</h1>            
+            <a href="#" class="btn btn-primary btn-lg">View Pricing</a>
+        </div>
+    </section>
+
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <aside class="p-4 bg-secondary value-added text-white p-5 text-center">
+                        <h4>Ready to streamline your fulfillment process?</h4>
+                        <h6>Connect with Fulfillment Ninjas today</h6>
+                        <div>
+                            email: <a href="mailto:info@fulfillmentninjas.com">info@fulfillmentninjas.com</a>
+                        </div>
+                    </aside>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <?php require 'includes/footer.php'; ?>
+
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"
+        integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
+
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"
+        integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="slick/slick.min.js"></script>
+
+    <script src="js/main.js"></script>
+</body>
+
+</html>
