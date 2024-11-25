e9cc3b751723f18f793b576adb2b0afdba74f2d9
cr8nvgitdemo <cr@8nvconsulting.com>
2024-11-26 03:37:27 +0530

    Contact us page added

---
+++ contact-us.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fulfillment Ninjas</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <link rel="stylesheet" type="text/css" href="slick/slick.css"/>
+    <link rel="stylesheet" type="text/css" href="slick/slick-theme.css"/>
+
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+  
+    <?php require 'includes/header.php'; ?>
+
+    <section class="shadow-box my-5">
+        <div class="container">
+            <aside class="p-0">
+                <div class="row">
+                    <div class="col-12 col-lg-4 p-5 text-start" style="background-color:var(--secondary-extralight)">
+                        <ul class="list-unstyled contact-data">
+                            <a href="#" class="d-flex gap-3 mb-3">
+                                <i class="fa-solid fa-envelope"></i>
+                                <div>
+                                    Email Us
+                                    <div class="fw-bold">contact@shiphype.com</div>
+                                </div>
+                            </a>
+                            <a href="#" class="d-flex gap-3 mb-3">
+                                <i class="fa-solid fa-phone"></i>
+                                <div>
+                                    Call Us
+                                    <div class="fw-bold">+1 844.745.1999</div>
+                                </div>
+                            </a>
+                            <a href="#" class="d-flex gap-3 mb-3 align-items-center">
+                                <i class="fa-solid fa-location-dot"></i>
+                                <div>         
+                                    <div class="fw-bold">Warehouse Locations</div>
+                                </div>
+                            </a>
+                        </ul>
+                    </div>
+                    <div class="col-12 col-lg-8 p-5">
+                        <div class="banner pt-0 text-start">
+                            <h1>Contact Us</h1>
+                            <div class="row">
+                                <div class="col-6 mb-3">
+                                    <label for="">Enter Name *</label>
+                                    <input class="form-control" type="text">
+                                </div>
+                                <div class="col-6 mb-3">
+                                    <label for="">Enter Name *</label>
+                                    <input class="form-control" type="text">
+                                </div>
+                                <div class="col-6 mb-3">
+                                    <label for="">Email *</label>
+                                    <input class="form-control" type="text">
+                                </div>
+                                <div class="col-6 mb-3">
+                                    <label for="">Phone *</label>
+                                    <input class="form-control" type="text">
+                                </div>
+                                <div class="col-12 mb-3">
+                                    <label for="">Message *</label>
+                                    <textarea class="form-control" name="" id=""></textarea>
+                                </div>
+                                <div class="col-12 d-grid">
+                                    <button class="btn btn-primary f-grow-1">Send Message</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </aside>
+        </div>
+    </section>
+
+    <?php require 'includes/footer.php'; ?>
+
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="slick/slick.min.js"></script>
+    
+    <script src="js/main.js"></script>
+  </body>
+</html>
