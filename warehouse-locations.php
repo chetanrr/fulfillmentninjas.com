c85bd179f270dfab10c79f26b107a1964dda5c49
cr8nvgitdemo <cr@8nvconsulting.com>
2024-11-26 02:51:43 +0530

    warehouse locations page added

---
+++ warehouse-locations.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fulfillment Ninjas</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <link rel="stylesheet" type="text/css" href="slick/slick.css"/>
+    <link rel="stylesheet" type="text/css" href="slick/slick-theme.css"/>
+
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+  
+    <?php require 'includes/header.php'; ?>
+
+    <section class="pt-2">
+        <div class="text-center banner">
+            <h1 class="mb-0">Warehouse Locations</h1>
+        </div>
+        <div class="container blogs">
+            <table class="service-summary-table w-100 table mb-5">
+                <thead>
+                    <tr class="header">
+                        <td colspan="2" class="p-0">
+                            <div class="bg-primary text-white h2 m-0 p-3">Toronto, Ontario, Canada</div>
+                        </td>
+                    </tr>
+                </thead>
+                <tbody>                    
+                    <tr>
+                        <td style="width:30%" class="pe-3 pt-5">
+                            <h3 class="text-primary">North York (Head Office)</h3>
+                            <p>3595 St Clair Ave E, Unit 15 Scarborough, ON M1K 1L8 Canada</p>
+                        </td>
+                        <td>
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d26538.214224405605!2d-117.94712956866006!3d33.75351297100697!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80dcd8757db69fd5%3A0xcf0589f30d70c27d!2sGarden%20Grove%2C%20CA%2092843%2C%20USA!5e0!3m2!1sen!2sin!4v1732567922645!5m2!1sen!2sin" width="100%" height="350" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+
+            <table class="service-summary-table w-100 table">
+                <thead>
+                    <tr class="header">
+                        <td colspan="2" class="p-0">
+                            <div class="bg-primary text-white h2 m-0 p-3">Toronto, Ontario, Canada</div>
+                        </td>
+                    </tr>
+                </thead>
+                <tbody>                    
+                    <tr>
+                        <td style="width:30%" class="pe-3 pt-5">
+                            <h3 class="text-primary">North York (Head Office)</h3>
+                            <p>3595 St Clair Ave E, Unit 15 Scarborough, ON M1K 1L8 Canada</p>
+                        </td>
+                        <td>
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d26538.214224405605!2d-117.94712956866006!3d33.75351297100697!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80dcd8757db69fd5%3A0xcf0589f30d70c27d!2sGarden%20Grove%2C%20CA%2092843%2C%20USA!5e0!3m2!1sen!2sin!4v1732567922645!5m2!1sen!2sin" width="100%" height="350" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </section>
+
+    <section class="pattern-block">
+        <div class="container">
+            <h1>Customized to Perfection</h1>
+            <p>Every Subscription Box Business has it's own set of unique requirements - and we understand that. We'll make sure every box goes out with the level of excellence your customers deserve.</p>
+            <a href="#" class="btn btn-primary btn-lg">View Pricing</a>
+        </div>
+    </section>
+
+    <?php require 'includes/footer.php'; ?>
+
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="slick/slick.min.js"></script>
+    
+    <script src="js/main.js"></script>
+  </body>
+</html>
