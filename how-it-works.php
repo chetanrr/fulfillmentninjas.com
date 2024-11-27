0626777be031e4b2ac662104d4edcc9d9a01ad3b
cr8nvgitdemo <cr@8nvconsulting.com>
2024-11-27 05:50:25 +0530

    how it works updated

--- how-it-works.php
+++ how-it-works.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Fulfillment Ninjas</title>
+  <title>Fulfillment Ninjas - How it Works</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
@@ -22,7 +22,8 @@
   <?php require 'includes/header.php'; ?>
 
   <section class="vid-banner text-center">
-    <video src="images/HowItWorks.mp4" autoplay muted loop></video>
+    <video src="images/HowItWorks.mp4" autoplay muted loop>
+    </video>
     <div class="container py-5 my-5">
       <h1 class="display-4 fw-bold">How it Works</h1>
       <h4>Ready to simplify fulfillment?</h4>
